0123b451266c45577f1cbe697c258f303441fb9d
Amine GABSI <mamine.gabsi@gmail.com>
2022-01-11 23:28:33 +0100

    Adding todo functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import { Component } from "react/cjs/react.production.min";
 import "./App.css";
+import Header from "./components/Header";
+import TodoInput from "./components/TodoInput"
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      todos: [
+        {id: 0, text: "Deliver Pizza"},
+        {id: 1, text: "Learn React TypeScript"},
+        {id: 2, text: "Take a shower"}
+      ],
+      nextId: 3
+    }
+
+    this.addTodo = this.addTodo.bind(this)
+    this.removeTodo = this.removeTodo.bind(this)
+  }
+  addTodo(todoText){
+    console.log("TODOS: " + todoText)
+  }
+  removeTodo(id){
+    console.log("Removed: " + id)
+  }
   render() {
     return (
       <div className="App">
-        <div className="todo-wrapper">Hello World! Let's do tasks</div>
+        <div className="todo-wrapper">
+          <Header />
+          <TodoInput todoText="" addTodo={this.addTodo}/>
+        </div>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Header extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h3>React todos</h3>
+        <hr></hr>
+      </div>
+    );
+  }
 }
