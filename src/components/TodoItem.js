a95d580c17764376dbf78b84a5759704c7b0eb8a
Amine GABSI <mamine.gabsi@gmail.com>
2022-01-12 12:34:32 +0100

    todoItem classes

---
+++ src/components/TodoItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./todoItem.css";
+
+export default class TodoItem extends Component {
+  removeTodo(id) {
+    this.props.removeTodo(id);
+  }
+
+  render() {
+    return (
+        <div className="todoWrapper">
+          <span>{this.props.todo.text}</span>
+          <button
+            className="btn btn-danger removeTodo"
+            onClick={() => {
+              this.removeTodo(this.props.id);
+            }}
+          >
+            Delete
+          </button>
+        </div>
+    );
+  }
+}
