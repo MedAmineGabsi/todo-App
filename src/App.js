b663b5ab8f60d234174b9eb66c74d5730b297995
Amine GABSI <mamine.gabsi@gmail.com>
2022-01-12 12:33:41 +0100

    adding render todo

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
 import { Component } from "react/cjs/react.production.min";
 import "./App.css";
 import Header from "./components/Header";
-import TodoInput from "./components/TodoInput"
+import TodoInput from "./components/TodoInput";
+import TodoItem from "./components/TodoItem";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       todos: [
-        {id: 0, text: "Deliver Pizza"},
-        {id: 1, text: "Learn React TypeScript"},
-        {id: 2, text: "Take a shower"}
+        { id: 0, text: "Deliver Pizza" },
+        { id: 1, text: "Learn React TypeScript" },
+        { id: 2, text: "Take a shower" },
       ],
-      nextId: 3
-    }
+      nextId: 3,
+    };
 
-    this.addTodo = this.addTodo.bind(this)
-    this.removeTodo = this.removeTodo.bind(this)
+    this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
   }
-  addTodo(todoText){
-    console.log("TODOS: " + todoText)
+  addTodo(todoText) {
+    console.log("TODOS: " + todoText);
   }
-  removeTodo(id){
-    console.log("Removed: " + id)
+  removeTodo(id) {
+    console.log("Removed: " + id);
   }
   render() {
     return (
       <div className="App">
         <div className="todo-wrapper">
           <Header />
-          <TodoInput todoText="" addTodo={this.addTodo}/>
+          <TodoInput todoText="" addTodo={this.addTodo} />
+          <li>
+            {this.state.todos.map((todo) => {
+              return (
+                <TodoItem
+                  todo={todo}
+                  key={todo.id}
+                  id={todo.id}
+                  removeTodo={this.removeTodo}
+                />
+              );
+            })}
+          </li>
         </div>
       </div>
     );
